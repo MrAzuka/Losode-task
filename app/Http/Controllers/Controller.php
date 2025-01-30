fde0c7543f002278abe9efe1e9509a7a94213894
MrAzuka <johntwice8@gmail.com>
2025-01-30 20:05:33 +0100

    add controllers

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,11 +2,45 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    /**
+     * success response method.
+     */
+    public function sendSuccessResponse($code, $message, $data = [])
+    {
+        $response = [
+            'status' => $code,
+            'message' => $message
+        ];
+        if (!empty($data)) {
+            $response['data'] = $data;
+        }
+        return new JsonResponse($response, $code);
+    }
+
+    /**
+     * error response method.
+     */
+
+    public function sendErrorResponse($code, $error, $errorMessages = [])
+    {
+        $response = [
+            'status' => $code,
+            'message' => $error,
+        ];
+
+        if (!empty($errorMessages)) {
+            $response['error-message'] = $errorMessages;
+        }
+
+        return new JsonResponse($response, $code);
+    }
 }
